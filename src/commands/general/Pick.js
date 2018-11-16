1c8f12ea73d4526b6262a9420893d60703294431
Lunerr <kyleroe94@gmail.com>
2018-11-15 22:40:19 -0600

    ping channel

--- src/commands/general/Pick.js
+++ src/commands/general/Pick.js
@@ -173,7 +173,7 @@ class Pick extends patron.Command {
 
       const embed = new MessageEmbed()
       .setColor(Random.arrayElement(Constants.data.colors.defaults))
-      .addField('Game Info', 'Lobby: ' + msg.channel.name + '\nGame: ' + msg.dbLobby.gamesPlayed)
+      .addField('Game Info', 'Lobby: <#' + msg.channel.name + '>\nGame: ' + msg.dbLobby.gamesPlayed)
       .addField('Team 1', team1)
       .addField('Team 2', team2.substring(0, team2.length - 2))
       .addField('Map', map)
