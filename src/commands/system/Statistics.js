72602275cdc175b872dfa9e15688ac738f8506dd
Lunerr <kyleroe94@gmail.com>
2019-03-27 06:12:53 -0500

    bot owner

--- src/commands/system/Statistics.js
+++ src/commands/system/Statistics.js
@@ -14,17 +14,24 @@ class Statistics extends patron.Command {
 
   async run(msg) {
     const uptime = NumberUtil.msToTime(msg.client.uptime);
-
     let users = 0;
 
     for (const guild of msg.client.guilds.values()) {
       users += guild.memberCount;
     }
 
-    await msg.author.DMFields([
-      'Authors', Constants.data.misc.botOwners.join('\n'), 'Framework', 'patron.js', 'Memory', (process.memoryUsage().rss / 1048576).toFixed(2) + ' MB', 'Servers', msg.client.guilds.size,
-      'Users', users, 'Uptime', 'Days: ' + uptime.days + '\nHours: ' + uptime.hours + '\nMinutes: ' + uptime.minutes
-    ]);
+    await msg.author.DMFields(["Authors",
+      msg.client.users.get('226736342745219072').tag,
+      "Framework",
+      "patron.js",
+      "Memory",
+      `${(process.memoryUsage().rss / 1048576).toFixed(2)} MB`,
+      "Servers",
+      msg.client.guilds.size,
+      "Users",
+      users,
+      "Uptime",
+      `Days: ${uptime.days}\nHours: ${uptime.hours}\nMinutes: ${uptime.minutes}`]);
 
     if (msg.channel.type !== 'dm') {
       return msg.createReply('you have been DMed with all ' + msg.client.user.username + ' Statistics!');
